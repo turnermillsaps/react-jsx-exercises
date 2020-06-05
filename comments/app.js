b3b0b1f77a3997254837077b46799e05eac45185
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-06-04 21:32:45 -0400

    Finished basics of blog and comments

--- comments/app.js
+++ comments/app.js
@@ -1,17 +1,29 @@
 let ProfilePic = () => {
-    return <span>Make this component render an image</span>
+    return <span>
+        <img src="Shoresy.jpg"></img>
+    </span>
 }
 
 let CommentBody = () => {
-    return <span>Make this component render a username in bold above the comment text</span>
+    return <span>
+        <h1>Shoresy</h1>
+    </span>
 }
 
 let Comment = () => {
-    return <span>Make each comment render a profile picture and the comment body</span>
+    return <span>
+        <ProfilePic />
+        <CommentBody />
+        <p>Give your balls a tug!</p>
+    </span>
 }
 
 let Comments = () => {
-    return <span>Make this component render a list of comments</span>
+    return <span>
+        <Comment />
+        <Comment />
+        <Comment />
+    </span>
 }
 
 ReactDOM.render(<Comments />, document.getElementById('root'))
--- form/app.js
+++ form/app.js
@@ -1,9 +1,21 @@
 let TextInput = () => {
-    return <span>Make this component render a styled text input tag</span>
+    return <span>
+        <div className="input-group input-group-sm mb-3">
+            <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-sm">Small</span>
+            </div>
+            <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"></input>
+        </div>
+    </span>
 }
 
 let YesNoRadio = () => {
-    return <span>Make this component render two styled radio button side by side labelled "yes" and "no"</span>
+    return <span>
+        <input type="radio" id="yes"></input>
+        <label for="yes">Yes</label>
+        <input type="radio" id="no"></input>
+        <label for="no">No</label>
+    </span>
 }
 
 let SubmitButton = () => {
